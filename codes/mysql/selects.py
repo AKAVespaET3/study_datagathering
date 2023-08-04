13343a021e384102208544928dcdd59092720a18
AKAVespaET3 <quattrini244@gmail.com>
2023-08-04 17:59:06 +0900

    first commit

---
+++ codes/mysql/selects.py
@@ -0,0 +1,13 @@
+# mysql connect package
+# pip --> conda
+
+# connect Mysql
+# ip, port, username, database name
+
+# editor - statement
+
+# execute select query 
+
+# return resultset and then display
+
+# close
